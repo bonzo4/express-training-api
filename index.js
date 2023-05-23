0d3c7a620b41bdbd45b8ff0a17e878c97e750ad2
hvKalista <alonzo.sabino814@gmail.com>
2023-05-23 15:11:19 -0500

    added post route

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 const express = require('express')
+const fs = require('fs')
 const app = express()
+
+app.use(express.json())
 
 const PORT = process.env.PORT || 4000
 
 app.get('/', (req, res) => {
     const data = require('./data/trains.json')
     res.send(data)
+})
+
+app.post('/', async (req, res) => {
+    const data = require('./data/trains.json')
+    data.push({
+        id: `${data.length + 1}`,
+        ...req.body
+    })
+    fs.writeFile('./data/trains.json', JSON.stringify(data), (err) => {
+        if (err) {
+            res.send(err)
+            return
+        }
+    })
+    res.send(req.body)
 })
 
 app.listen(PORT, () => {
